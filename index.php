3667055e0fcadf811c4d3803a51f6ac163449b96
Gianni Narracci <gnarracci@gmail.com>
2021-03-02 17:54:19 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="Website to publish Classified Announcements for any purpose build on Php, JQuery, Bootstrap">
+	<link rel="shortcut icon" href="img/classified.ico" type="image/x-icon">
+	<title>Classified Test Roostack</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	
+</body>
+</html>

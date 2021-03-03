4ff7a1b7964bcfc1db84a38ff55194c1677ef1f8
Gianni Narracci <gnarracci@gmail.com>
2021-03-03 19:45:46 -0400

    ScrappingApp Added to the Project

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="Website to publish Classified Announcements for any purpose build on Php, JQuery, Bootstrap">
 	<link rel="shortcut icon" href="img/classified.ico" type="image/x-icon">
-	<title>Classified Test Roostack</title>
+	<title>Classified Advertisement</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
